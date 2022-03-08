dbcd89001f9e23b14c65bb562177b3e51c4412b5
acappy99 <71903100+acappy99@users.noreply.github.com>
2022-03-08 11:09:10 -0500

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+from time import sleep
 import numpy as np
 import pandas as pd
 import datetime as datetime
@@ -5,9 +7,19 @@ import pickle
 import matplotlib.pyplot as plt
 import mgmt
 
-path = input('Path: ')
+# constant variables
+report_path = '/Users/alexcappadona/Desktop/JARVIS/output/data.xlsx'
+aaa_path = '/Users/alexcappadona/Desktop/JARVIS/aaa/'
+
+words = "Welcome to SMIF_Jarvis\n"
+for char in words:
+    sleep(0.1)
+    print(char, end='', flush=True)
+
+print('')
+print('Asset and accrual')
 fname = input('Filename: ')
-loc = path+fname
+loc = aaa_path+fname
 
 # total portfolio dataframe
 ass = pd.read_excel(loc)
@@ -25,26 +37,70 @@ print('')
 
 # Record current pd AUM in record, takes aum dataframe
 # Records with date as current date from datetime()
-print('writing to AUM history...')
-mgmt.record_aum(aum)
-print('')
+print('Add current AUM to history? (y/n)')
+selection = input('>')
+if selection == 'y':
+    words = "Writing to AUM history...\n"
+    for char in words:
+        sleep(0.1)
+        print(char, end='', flush=True)
+    mgmt.update_aum(aum)
+    print('')
 
 # Show AUM history
 print('')
-aum_record = mgmt.get_aum_record()
+aum_frame = mgmt.get_aum_record()
 print('AUM History')
-print(aum_record)
+print(aum_frame)
 print('')
 
-# create DataFrame of only securities
-print('Fields to drop, separated by enter, type "done" when finished.')
-drop_lst = []
-while True:
-    drops = input('>')
-    if drops == 'done':
-        break
-    drops = int(drops)
-    drop_lst.append(drops)
-securities = mgmt.drop_fields(assets, drop_lst)
-print(securities)
+# AUM delta $
+print('One day ($) change in AUM:')
+aum_delta = mgmt.aum_delta(aum_frame)
+print(aum_delta)
+print('')
 
+# AUM delta %
+print('One day (%) change in AUM:')
+aum_delta_per = mgmt.aum_delta_per(aum_frame)
+print(aum_delta_per)
+print('')
+
+# AUM graph
+print('View AUM chart? (y/n)')
+selection = input('>')
+print('')
+if selection == 'y':
+    words = "Creating AUM chart...\n"
+    for char in words:
+        sleep(0.1)
+        print(char, end='', flush=True)
+    mgmt.aum_graph(aum_frame)
+
+# Create DataFrame of only securities
+print('Create DataFrame of securities (y/n)')
+selection = input('>')
+print('')
+
+if selection == 'y':
+    print('Fields to drop, separated by enter, type "done" when finished.')
+    drop_lst = []
+    while True:
+        drops = input('>')
+        if drops == 'done':
+            break
+        drops = int(drops)
+        drop_lst.append(drops)
+    securities = mgmt.drop_fields(assets, drop_lst)
+    print(securities)
+
+print('')
+print('Generate report? (y/n)')
+selection = input('>')
+if selection == 'y':
+    words = "Generating report...\n"
+    for char in words:
+        sleep(0.1)
+        print(char, end='', flush=True)
+    mgmt.gen_report(report_path, assets, securities, aum_frame)
+    print('Success')
